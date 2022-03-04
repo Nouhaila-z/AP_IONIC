ad3171416418cd6660840bb57479ffa1137b1185
Zabili Nouhaila <nouhaila.zbl@gmail.com>
2022-03-04 16:14:14 +0100

    initial push

---
+++ AP_cerza/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AP_cerza',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
