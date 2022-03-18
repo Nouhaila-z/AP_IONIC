3bf5c8e51caaa1fc4eb49de71cff662d97ac6846
Lilian Vasconcelos <lilianvscls@gmail.com>
2022-03-18 15:30:55 +0100

    <feature>(alertes): Delete

--- AP_cerza/src/app/alertes/alertes.page.ts
+++ AP_cerza/src/app/alertes/alertes.page.ts
@@ -20,20 +20,20 @@ export class AlertesPage {
     return this.http.get(URL);
   }
 
-afficherAlertes(){
-  this.alertes = [];
-  this.readAPI('http://localhost:3000/alertes')
-  .subscribe((data) => {
-    for (let i = 0; i < data['data'].length; i++) {
-      this.alertes.push(data['data'][i]);
-    }
-    console.log(this.alertes);
-  });
-}
+  afficherAlertes() {
+    this.alertes = [];
+    this.readAPI('http://localhost:3000/alertes')
+      .subscribe((data) => {
+        for (let i = 0; i < data['data'].length; i++) {
+          this.alertes.push(data['data'][i]);
+        }
+        console.log(this.alertes);
+      });
+  }
 
-ngOnInit(){
-  this.afficherAlertes();
-}
+  ngOnInit() {
+    this.afficherAlertes();
+  }
   addAlerte() {
     this.modalCtrl.create({ component: AjouterAlertePage }).then(modalres => { modalres.present(); modalres.onDidDismiss().then(res => { if (res.data != null) { this.afficherAlertes(); } }) })
   }
@@ -41,6 +41,41 @@ ngOnInit(){
     this.modalCtrl.create({ component: ModifierAlertePage, componentProps: alerte }).then(modalres => { modalres.present(); modalres.onDidDismiss().then(res => { if (res.data != null) { this.afficherAlertes(); } }) })
   }
 
+  async DeleteAlerte (id, libelle)
+  {
+  await this.alertController.create({
+
+    header: "Supprimer l'alerte",
+
+    subHeader: libelle,
+
+    message: "Voulez-vous vraiment supprimer l'alerte ?",
+
+    buttons: [
+
+      {
+        text: "Oui", handler: () => {
 
 
-}
+
+          this.http.delete(`http://localhost:3000/alertes/${id}`,).subscribe((res: any) => {
+            this.afficherAlertes();
+
+            this.toastCtrl.create({ duration: 3000, message: "L'alerte à bien été supprimée." }).then(t => t.present());
+
+          });
+
+        }
+
+      },
+
+      {
+
+        text: "Non"
+
+      }
+    ]
+
+  }).then(res => res.present());
+
+}}
